43a6045e44e08a244a402ad2eb91871d782dee3a
Cloxy7 <jillamahima@gmail.com>
2026-01-13 09:36:07 +0530

    Add AST-based code analyzer with JSON output

--- my_ast_analyzer.py
+++ my_ast_analyzer.py
@@ -1,27 +1,27 @@
-import ast
-import json
+import ast  #python built-in module
+import json # need for the output format
 
 with open("sample.py", "r") as f:
-    code = f.read()
+    code = f.read()   #reads its entire contents as a string.
 
-tree = ast.parse(code)
+tree = ast.parse(code) #converting the source code(sample.py) into AST
 
-functions = []
+functions = [] # empty lists to store information
 classes = []
 imports = []
 
-for node in ast.walk(tree):
-    if isinstance(node, ast.FunctionDef):
+for node in ast.walk(tree):  # for every node in the tree (ast.walk traverses the entire tree)
+    if isinstance(node, ast.FunctionDef): #Whether the current node represents a function definition.
         functions.append({
             "name": node.name,
             "line": node.lineno
         })
-    elif isinstance(node, ast.ClassDef):
+    elif isinstance(node, ast.ClassDef): #Whether the node represents a class definition.
         classes.append({
             "name": node.name,
             "line": node.lineno
         })
-    elif isinstance(node, ast.Import):
+    elif isinstance(node, ast.Import): # This records which external modules the source code depends on.
         imports.extend([n.name for n in node.names])
 
 
